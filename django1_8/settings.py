d8f53eb9e458badb9a0912259baf5c66e3d3f3c7
pac <dangi.sushil5@gmail.com>
2018-02-11 17:56:51 +0530

    mail Setup continue....

--- django1_8/settings.py
+++ django1_8/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='dangi.sushil4@gmail.com'
-EMAIL_HOST_PASSWORD='password'
+EMAIL_HOST_USER='sushil@pirsq.com'
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 from newsletter.forms import SignUpForm, ContactForm
@@ -28,7 +29,19 @@ def home(request):
 def contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        print(form.cleaned_data)
+        form_email = form.cleaned_data.get('email')
+        form_message = form.cleaned_data.get('message')
+        form_full_name = form.cleaned_data.get('full_name')
+        subject = 'Site contact Form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = ['dangi.sushil5@gmail.com', 'sushil@pacdevelopers.com']
+        contact_message = '%s : %s via %s' % (form_email, form_message, form_full_name)
+        send_mail(subject,
+                  contact_message,
+                  from_email,
+                  to_email,
+                  fail_silently=True)
+
     context={
         'form':form,
     }
