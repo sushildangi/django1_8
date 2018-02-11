27f4d63f2918eedb8b7ccd3e7e3b98f1ea8d07bf
pac <dangi.sushil5@gmail.com>
2018-02-11 17:08:43 +0530

    custom form data save

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from newsletter.forms import SignUpForm
 
 
 def home(request):
-    return render(request, "home.html", {})
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        'form': form,
+        'title': 'Welcome to Sign Up Page'
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        print(instance)
+
+        context = {
+            'title': 'Thank You :)'
+        }
+
+    return render(request, "home.html", context)
