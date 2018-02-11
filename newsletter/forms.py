7ea779c2d9c12a7b3b520ca7dc13c5c65c4c25ad
pac <dangi.sushil5@gmail.com>
2018-02-11 16:22:55 +0530

    custom form validation

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,3 +6,17 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['email','full_name']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extension = provider.split('.')
+        if not extension == "edu":
+            raise forms.ValidationError('Please Enter valid college email address!')
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        # validation
+
+        return full_name
