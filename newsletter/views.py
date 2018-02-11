10107c430bb938bc733f5e054fe59d46ff551d9b
pac <dangi.sushil5@gmail.com>
2018-02-11 22:50:02 +0530

    template inheritance

--- newsletter/views.py
+++ newsletter/views.py
@@ -23,7 +23,7 @@ def home(request):
             'title': 'Thank You :)'
         }
 
-    return render(request, "base.html", context)
+    return render(request, "home.html", context)
 
 
 def contact(request):
